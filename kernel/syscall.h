146c845cba9097bf18bb1565a142e7852b5d8ef1
0xAngoone <144622489+0xangoone@users.noreply.github.com>
2024-06-24 20:04:39 +0100

    Delete kernel/syscall.h

--- kernel/syscall.h
+++
@@ -1,7 +0,0 @@
-// syscall table functions
-
-typedef void (*PrintFunction)(const char*);
-PrintFunction tty_print = (PrintFunction)(0x1000 + 3);
-
-typedef void (*PrintCharFunction)(char);
-PrintCharFunction tty_char_print = (PrintCharFunction)(0x1000 + 8);
